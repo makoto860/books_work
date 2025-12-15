fce16d3751b549f35a1c6c3025abf0542488c7ad
下館諒 <shimodatemakoto@shimodateryounoMacBook-Air.local>
2025-12-15 16:57:05 +0900

    books_specifications/indexを追加

--- app/controllers/book_specifications_controller.rb
+++ app/controllers/book_specifications_controller.rb
@@ -1,4 +1,24 @@
 class BookSpecificationsController < ApplicationController
   def index
+    @book_specifications = BookSpecification.all
+  end
+
+  def new
+    @book_specification = BookSpecification.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
