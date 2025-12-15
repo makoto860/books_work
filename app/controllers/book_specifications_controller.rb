335f694da70c0bc9f5bb1e423c4d2a17d76e3fbe
下館諒 <shimodatemakoto@shimodateryounoMacBook-Air.local>
2025-12-15 18:11:05 +0900

    book_specifications/show.html.erbを作成

--- app/controllers/book_specifications_controller.rb
+++ app/controllers/book_specifications_controller.rb
@@ -8,9 +8,17 @@ class BookSpecificationsController < ApplicationController
   end
 
   def create
+    @book_specification = BookSpecification.new(params.require(:book_specification).permit(:title, :number_of_copies, :number_of_machines, :note, :deadline, :author))
+    if @book_specification.save
+      flash[:notice] = "仕様書を新規登録しました"
+      redirect_to :book_specifications
+    else
+      render "new"
+    end
   end
 
   def show
+    @book_specification = BookSpecification.find(params[:id])
   end
 
   def edit
