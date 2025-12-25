ea58465e9d8ccb54a7fb3bdf0bf62388a49e4a90
下館諒 <shimodatemakoto@shimodateryounoMacBook-Air.local>
2025-12-25 14:56:19 +0900

    レイアウトのアプリケーションのリンク先を設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  helper_method :current_book_specification
+  
+  def current_book_specification
+    @current_book_specification ||= begin
+      if params[:book_specification_id]
+        BookSpecification.find_by(id: params[:book_specification_id])
+      elsif controller_name == "book_specifications" &&
+        params[:id].present? &&
+        %w[show edit update destroy].include?(action_name)
+        BookSpecification.find_by(id: params[:id])
+      end
+    end
+  end
 end
