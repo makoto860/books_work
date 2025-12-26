e4e31c04da260fd7324962005e00afbabb438bdb
下館諒 <shimodatemakoto@shimodateryounoMacBook-Air.local>
2025-12-26 12:59:45 +0900

    バリデーションの追加

--- app/controllers/work_progresses_controller.rb
+++ app/controllers/work_progresses_controller.rb
@@ -1,8 +1,9 @@
 class WorkProgressesController < ApplicationController
   before_action :set_book_specification
+  before_action :set_work_progress, only: [:edit, :update]
 
   def index
-    @work_progresses = @book_specification.work_progresses
+    @work_progresses = WorkProgress.all
   end
 
   def new
@@ -17,10 +18,25 @@ class WorkProgressesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @work_progress.update(work_progress_params)
+      redirect_to book_specification_work_progresses_path(@book_specification), notice: "作業進捗を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
     def set_book_specification
       @book_specification = BookSpecification.find(params[:book_specification_id])
+    end
+
+    def set_work_progress
+      @work_progress = @book_specification.work_progresses.find(params[:id])
     end
 
     def work_progress_params
--- app/models/book_specification.rb
+++ app/models/book_specification.rb
@@ -1,7 +1,13 @@
 class BookSpecification < ApplicationRecord
   has_many :work_progresses, dependent: :destroy
 
-  def work_progresses_new
-    work_progresses.new
+  validates :title, presence: true
+  validates :number_of_copies, presence: true
+  validates :number_of_machines, presence: true
+  validates :deadline, presence: true
+  validates :author, presence: true
+
+  def deadline_today
+    errors.add(:deadline, "納期は今日以降の日付を選択してください") if deadline < check_in_date
   end
 end
