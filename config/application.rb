c0715f3bd9839dce7d97ab63c52fb4d6ac263449
下館諒 <shimodatemakoto@shimodateryounoMacBook-Air.local>
2026-01-16 12:41:38 +0900

    application.scssをビルド、config/application.rbでbuilds pathを追加

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module BooksWorkApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = "Tokyo"
+    config.assets.paths << Rails.root.join("app/assets/builds")
   end
 end
