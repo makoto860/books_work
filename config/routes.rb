0733a6757267023fb87bdd78490ad61ae8e9b0e1
下館諒 <shimodatemakoto@shimodateryounoMacBook-Air.local>
2026-01-20 16:48:50 +0900

    ルート先をログイン画面に設定

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
@@ -16,5 +17,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "homes#top"
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 end
